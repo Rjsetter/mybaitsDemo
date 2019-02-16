e7afc39e9aa185efbf1277cdb7a47975c3700191
RJSETTER <1039339929@qq.com>
2019-02-17 07:52:08 +0800

    添加整套USER的增删改查！

---
+++ src/main/java/com/yq/mybaits/MybaitsTest.java
@@ -0,0 +1,37 @@
+package com.yq.mybaits;
+
+import java.io.InputStream;
+
+import com.yq.mybaits.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+/**
+ * mybaits测试
+ * @author RJSETTER
+ * @date 2019/2/17
+ */
+public class MybaitsTest {
+    public static void main(String []args)throws Exception{
+        //指定全局配置文件
+        String resource = "mybaits-config.xml";
+        //读取配置文件
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        //构建sqlSessionFactory
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        //获取SqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        try {
+            // 操作CRUD，第一个参数：指定statement，规则：命名空间+“.”+statementId
+            // 第二个参数：指定传入sql的参数：这里是用户id
+            User user = sqlSession.selectOne("MyMapper.selectUser", 1);
+            User user1 = sqlSession.selectOne("MyMapper.selectUser",2);
+            System.out.println(user);
+            System.out.println("TEST:"+user1);
+        } finally {
+            sqlSession.close();
+        }
+    }
+}
---
+++ src/main/java/com/yq/mybaits/dao/impl/UserDaoImpl.java
@@ -0,0 +1,39 @@
+package com.yq.mybaits.dao.impl;
+
+import com.yq.mybaits.dao.UserDao;
+import com.yq.mybaits.pojo.User;
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.List;
+
+public class UserDaoImpl implements UserDao {
+    //创建SqlSession
+    public SqlSession sqlSession;
+
+    //析构函数
+    public UserDaoImpl(SqlSession sqlSession){
+        this.sqlSession = sqlSession;
+    }
+
+
+    public User queryUserById(int id){
+        return this.sqlSession.selectOne("UserDao.queryUserById",id);
+    }
+
+    public List<User> queryUserAll(){
+        return this.sqlSession.selectList("UserDao.queryUserAll");
+    }
+
+    public void insertUser(User user) {
+        this.sqlSession.insert("UserDao.insertUser", user);
+    }
+
+    public void updateUser(User user) {
+        this.sqlSession.update("UserDao.updateUser", user);
+    }
+
+    public void deleteUser(int id) {
+        this.sqlSession.delete("UserDao.deleteUser", id);
+    }
+
+}
