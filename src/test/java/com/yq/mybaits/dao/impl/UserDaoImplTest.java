7e42bb40a908c63ba6b0cd4ee173ea625d7f558b
RJSETTER <1039339929@qq.com>
2019-02-17 13:00:30 +0800

    动态代理

--- src/test/java/com/yq/mybaits/dao/impl/UserDaoImplTest.java
+++ src/test/java/com/yq/mybaits/dao/impl/UserDaoImplTest.java
@@ -1,33 +1,81 @@
 package com.yq.mybaits.dao.impl;
 
+import com.yq.mybaits.dao.UserDao;
+import com.yq.mybaits.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.InputStream;
+import java.util.Date;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class UserDaoImplTest {
 
+    public UserDao userDao;
+    public SqlSession sqlSession;
+
     @Before
     public void setUp() throws Exception {
+        // mybaits-config.xml
+        String resource = "mybaits-config.xml";
+        // 读取配置文件
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        // 构建SqlSessionFactory
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        //获取sqlSession
+        sqlSession = sqlSessionFactory.openSession();
+//        this.userDao = new UserDaoImpl(sqlSession);
     }
 
     @Test
-    public void queryUserById() {
+    public void queryUserById() throws Exception {
+        System.out.println(this.userDao.queryUserById(1));
     }
 
     @Test
-    public void queryUserAll() {
+    public void queryUserAll() throws Exception {
+        List<User> userList = this.userDao.queryUserAll();
+        for (User user : userList) {
+            System.out.println(user);
+        }
     }
 
     @Test
-    public void insertUser() {
+    public void insertUser() throws Exception {
+        User user = new User();
+        user.setAge(16);
+        user.setBirthday(new Date("1990/09/02"));
+        user.setName("大鹏");
+        user.setPassword("123456");
+        user.setSex(1);
+        user.setUserName("evan");
+        this.userDao.insertUser(user);
+        this.sqlSession.commit();
     }
 
     @Test
-    public void updateUser() {
+    public void updateUser() throws Exception {
+        User user = new User();
+        user.setBirthday(new Date());
+        user.setName("静鹏");
+        user.setPassword("654321");
+        user.setSex(1);
+        user.setUserName("evanjin");
+        user.setId("4");
+        this.userDao.updateUser(user);
+        this.sqlSession.commit();
     }
 
     @Test
-    public void deleteUser() {
+    public void deleteUser() throws Exception {
+        this.userDao.deleteUser(4);
+        this.sqlSession.commit();
     }
+
 }
